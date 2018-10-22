6bdd12e5353642b7af6ebd54a2963ac1a8d32c1e
Daniel Munoz <daniel.munozdev@gmail.com>
2018-10-21 19:06:00 -0700

    add fetch functionality to get subreddit posts

--- main.js
+++ main.js
@@ -1,8 +1,63 @@
-fetch('https://www.reddit.com/r/leagueoflegends.json')
+// disable form
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+});
+
+// fetch subreddit posts
+fetch('https://www.reddit.com/r/programmerhumor.json')
   .then(function (response) {
     return response.json();
   }).then(function (json) {
-    console.log(json);
     console.log(json.data.children);
-    return json.data.children;
+    populatePosts(json.data.children);
   });
+
+// populate and append subreddit posts to dom
+function populatePosts (array) {
+  // console.log(array);
+  let data = [];
+  // create an array of objects of elements to append later
+  array.forEach(function (object) {
+    data.push({
+      data: object.data.title,
+      element: 'h1'
+    });
+
+    if (object.data.url.includes('jpg')) {
+      console.log(data);
+      data.push({
+        data: object.data.preview.images[0].source.url,
+        element: 'img'
+      });
+    } else {
+      data.push({
+        data: 'no image',
+        element: 'div'
+      })
+    }
+    // console.log(data);
+    // append posts to dom
+    createAndAppendElements(data, object.data.permalink);
+    data = [];
+  });
+}
+
+function createAndAppendElements (array, source) {
+  // create wrapper for each post
+  let a = document.createElement('a');
+  a.href = 'https://www.reddit.com/' + source;
+
+  // create post title and img
+  array.forEach(function (object) {
+    let element = document.createElement(object.element);
+    if (object.element == 'img') {
+      element.source = object.data;
+    } else {
+      element.textContent = object.data;
+    }
+    // append title and img to wrapper
+    a.appendChild(element);
+    // append wrapper to dom
+    document.querySelector('#posts').appendChild(a);
+  });
+}
